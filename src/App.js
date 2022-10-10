4cc80036e60902417d528d4ee5155a92b3e134f7
Deepak <dipak280601@gmail.com>
2022-10-10 10:00:20 +0530

    Please enter the commit message for your changes. Lines starting  save->Reload to reloaded is done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Reload.
+          Edit <code>src/App.js</code> and save to Reload to reloated is done
         </p>
         <a
           className="App-link"
